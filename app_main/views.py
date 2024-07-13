52695c22c2f84d85dcd0bd9338700becdc189235
Jahongir <jahongirq702@gmail.com>
2024-07-14 02:21:35 +0500

    google

---
+++ app_main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+
+
+def home(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    return render(request, "app_main/home.html")
